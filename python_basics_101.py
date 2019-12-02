c638cde7d5801c46a3facda713b56c735b18070d
Thomas Nguyen <TNguyen@spartaglobal.com>
2019-12-02 12:48:40 +0000

    Added information on variables - assigning, calling and printing them

---
+++ python_basics_101.py
@@ -0,0 +1,26 @@
+# Variables
+## It's like a box
+##It has a name and has stuff inside
+## you can REUSE THE BOX
+
+
+## assignment of variable
+example_variable = 'hello the eng 48'
+
+### calling variable
+print(example_variable)
+
+# re assign what is inside the box
+example_variable = 48
+print(example_variable)
+
+# Print statement --> outputs to console
+# Print with type()
+
+example_variable2  = '48'
+
+print(type(example_variable))
+print(example_variable)
+print('/////')
+print(type(example_variable2))
+print(example_variable2)
