d4ea6d86a471dace4cfa009b427237676aaaa374
Thomas Nguyen <TNguyen@spartaglobal.com>
2019-12-03 11:39:37 +0000

    Done while loops

---
+++ python_basics_109_while_if.py
@@ -0,0 +1,27 @@
+# While loops
+
+# Syntax
+# while <condition>:
+    # block of code
+    # block of code
+    #Condition
+        # Break
+
+num = 0
+while num < 10:
+    print('still less than 10')
+    num += 1
+    
+wish_list = ('rc car', 'molten cheese', 'cheerleaders', 'White shirts',
+             'sugar laces', 'reeces', 'pastel de nata')
+
+# # Using to substitute the for loop
+# print(len(wish_list))
+# index_length = len(wish_list)-1
+# counter = 0 #index is at 0 that's why we -1 beforehand
+# while counter <= index_length:
+#     print(wish_list[counter])
+#     counter += 1
+
+
+
